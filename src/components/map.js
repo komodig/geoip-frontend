f7bdc9f8e4675b7a6f7a3131d1d50963737353a7
Lutz Ballaschke <l.ballaschke@tastybyte.de>
2021-12-26 10:05:37 +0100

    creating info box element for mouse-over nation

--- src/components/map.js
+++ src/components/map.js
@@ -1,10 +1,21 @@
-export function classHighlight(classid) {
-    console.log(classid)
+export function classHighlight(classid, dim) {
+    console.log(classid);
     var country = document.getElementsByClassName(classid);
     var x;
     for(x = 0; country[x] != null; x += 1) {
         country[x].style.fill = "rgb(60,230,60)";
     }
+    var infoBox = document.createElement("rect");
+    infoBox.id = "nation-info";
+    var pArr = dim.split(" ", 3);
+    console.log("x: " + pArr[1] + " y: " + pArr[2]);
+    infoBox.setAttribute("x", pArr[1]);
+    infoBox.setAttribute("y", pArr[2]);
+    infoBox.setAttribute("width", "60px");
+    infoBox.setAttribute("height", "20px");
+    infoBox.setAttribute("border-radius", "6px");
+    infoBox.setAttribute("background", "yellow");
+    document.getElementById("world-map").appendChild(infoBox);
 }
 
 export function classReset(classid) {
@@ -13,4 +24,6 @@ export function classReset(classid) {
     for(x = 0; country[x] != null; x += 1) {
         country[x].style.fill = "rgb(40,170,10)";
     }
+    //var infoBox = document.getElementById("nation-info");
+    //var container = document.getElementById("world-map").removeChild(infoBox);
 }
